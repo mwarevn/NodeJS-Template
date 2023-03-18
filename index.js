6520ff4fa5344ee55718b303356504890fe562e2
minhdevs <minhnvph21862@fpt.edu.vn>
2023-03-19 03:53:35 +0700

    modified:   index.js 	modified:   node_modules/.package-lock.json 	deleted:    node_modules/asap/CHANGES.md 	deleted:    node_modules/asap/LICENSE.md 	deleted:    node_modules/asap/README.md 	deleted:    node_modules/asap/asap.js 	deleted:    node_modules/asap/browser-asap.js 	deleted:    node_modules/asap/browser-raw.js 	deleted:    node_modules/asap/package.json 	deleted:    node_modules/asap/raw.js 	deleted:    node_modules/dezalgo/LICENSE 	deleted:    node_modules/dezalgo/README.md 	deleted:    node_modules/dezalgo/dezalgo.js 	deleted:    node_modules/dezalgo/package.json 	deleted:    node_modules/formidable/CHANGELOG.md 	deleted:    node_modules/formidable/LICENSE 	deleted:    node_modules/formidable/README.md 	deleted:    node_modules/formidable/package.json 	deleted:    node_modules/formidable/src/Formidable.js 	deleted:    node_modules/formidable/src/FormidableError.js 	deleted:    node_modules/formidable/src/PersistentFile.js 	deleted:    node_modules/formidable/src/VolatileFile.js 	deleted:    node_modules/formidable/src/index.js 	deleted:    node_modules/formidable/src/parsers/Dummy.js 	deleted:    node_modules/formidable/src/parsers/JSON.js 	deleted:    node_modules/formidable/src/parsers/Multipart.js 	deleted:    node_modules/formidable/src/parsers/OctetStream.js 	deleted:    node_modules/formidable/src/parsers/Querystring.js 	deleted:    node_modules/formidable/src/parsers/StreamingQuerystring.js 	deleted:    node_modules/formidable/src/parsers/index.js 	deleted:    node_modules/formidable/src/plugins/index.js 	deleted:    node_modules/formidable/src/plugins/json.js 	deleted:    node_modules/formidable/src/plugins/multipart.js 	deleted:    node_modules/formidable/src/plugins/octetstream.js 	deleted:    node_modules/formidable/src/plugins/querystring.js 	deleted:    node_modules/hexoid/dist/index.js 	deleted:    node_modules/hexoid/dist/index.min.js 	deleted:    node_modules/hexoid/dist/index.mjs 	deleted:    node_modules/hexoid/hexoid.d.ts 	deleted:    node_modules/hexoid/license 	deleted:    node_modules/hexoid/package.json 	deleted:    node_modules/hexoid/readme.md 	deleted:    node_modules/once/LICENSE 	deleted:    node_modules/once/README.md 	deleted:    node_modules/once/once.js 	deleted:    node_modules/once/package.json 	deleted:    node_modules/wrappy/LICENSE 	deleted:    node_modules/wrappy/README.md 	deleted:    node_modules/wrappy/package.json 	deleted:    node_modules/wrappy/wrappy.js 	modified:   package-lock.json 	modified:   package.json 	new file:   src/controllers/HomeController.js 	deleted:    src/css/style.css 	deleted:    src/css/style.css.map 	modified:   src/css/style.scss 	new file:   src/routes/home.js 	new file:   src/routes/index.js 	deleted:    views/home.hbs 	new file:   views/index.hbs 	modified:   views/layouts/main.hbs 	modified:   views/partials/footer.hbs 	modified:   views/partials/header.hbs

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const fs = require('fs')
-const path = require('path')
-const hbs = require('express-handlebars')
+const hbs = require('express-handlebars');
+const route = require('./src/routes');
 
-app.engine('hbs', hbs.engine({extname: '.hbs'}));
+app.engine('hbs', hbs.engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', './views');
-
 app.use(express.json());
-app.use(express.static(__dirname + '/src'))
+app.use(express.static(__dirname + '/src'));
+
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
-  console.log("Application started and Listening on port 3000");
+    console.log('Application started and Listening on port 3000 ...');
 });
 
-app.get("/", (req, res) => {
-  res.render('home')
-});
+route(app);
---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+const home = require('./home');
+
+function route(app) {
+    app.use('/', home);
+}
+
+module.exports = route;
